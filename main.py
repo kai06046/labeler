eb5bd4107a2e394692e5d08e2ad6891bba828688
afunTW <endlessproof@gmail.com>
2018-04-11 02:01:46 +0800

    mod: indention

--- main.py
+++ main.py
@@ -32,10 +32,10 @@ def log_handler(loggers):
 
 if __name__ == '__main__':
 	logging.basicConfig(
-		   level=logging.INFO,
-		   format='%(asctime)s %(filename)12s:L%(lineno)3s [%(levelname)8s] %(message)s',
-		   datefmt='%Y-%m-%d %H:%M:%S',
-		   stream=sys.stdout
+		level=logging.INFO,
+		format='%(asctime)s %(filename)12s:L%(lineno)3s [%(levelname)8s] %(message)s',
+		datefmt='%Y-%m-%d %H:%M:%S',
+		stream=sys.stdout
 	)
 	log_handler(LOGGERS)
 	labeler = Labeler()
